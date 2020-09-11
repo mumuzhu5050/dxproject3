a087be18648339bfeb24083a56485f283e2876b2
mumuzhu <15305602005@189.cn>
2020-09-11 09:38:26 +0800

    初始化仓库

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    watch:true,
+    mode:'development',
+    entry:{
+        index:'./src/index.js'
+    },
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname,'src')
+    },
+    module:{
+        rules:[
+            { 
+                test: /\.js$/,
+                use: 'babel-loader' 
+            },
+            { 
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            { 
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: 'file-loader'
+            },
+            { 
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: 'file-loader'
+            },
+        ]
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer:{
+        host:'127.0.0.1',
+        port:'8080',
+        hot:true,
+        contentBase:'./src'
+    }
+}
